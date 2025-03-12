902917cb2836bff7bd310b577b2182cc6ac5bfb2
Jia De <jiadepang@gmail.com>
2025-03-12 18:47:00 +0800

    Update

--- Binary Search Tree/BST.py
+++ Binary Search Tree/BST.py
@@ -4,7 +4,26 @@ class bst:
         self.lchild = None
         self.rchild = None
 
+    def insert(self,data):
+        if self.key is None:
+            self.key = data
+            return
+        if self.key == data:
+            return
+        if self.key > data:
+            if self.lchild:
+                self.lchild.insert(data)
+            else:
+                self.lchild = bst(data)
+        else:
+            if self.rchild:
+                self.rchild.insert(data)
+            else:
+                self.rchild = bst(data)
+
+
 root = bst(10)
-print(root.key)
-print(root.lchild)
-print(root.rchild)
+list1 = [20,4,30,4,1,5,6]
+for i in list1:
+    root.insert(i)
+
