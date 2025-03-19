52e54db83d0e385c4b9ebf1b1ca189b7a226bbcc
Jia De <jiadepang@gmail.com>
2025-03-19 17:07:05 +0800

    1

---
+++ Binary Search Tree/insert.py
@@ -0,0 +1,11 @@
+class BST:
+    def __init__(self,key):
+        self.key = key
+        self.lchild = None
+        self.rchild = None
+
+    def insert(self,data):
+
+
+root = BST(10)
+root.insert(20)
